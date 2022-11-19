54280a6d0fcd401b1fd0e51fac7a5c536112098c
Osi, Keme <108202263+Kemelite1@users.noreply.github.com>
2022-11-19 12:11:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const heading = document.querySelector('h1');
 const disImg = document.querySelector('.displayed-img');
 
-const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'];
-const altText = ['img1', 'img2', 'img3', 'img4', 'img5'];
+const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg' 'img6.jpg'];
+const altText = ['img1', 'img2', 'img3', 'img4', 'img5' 'img6'];
 
 document.querySelector('.prev').addEventListener('click', () => {
     const imageSrc = disImg.getAttribute('src')
-    const i = (images.indexOf(imageSrc.split('/')[1]) - 1 + 5) % 5;
+    const i = (images.indexOf(imageSrc.split('/')[1]) - 1 + 6) % 6;
     const image = `img/${images[i]}`;
 
     // disImg.setAttribute('src', image);
@@ -17,7 +17,7 @@ document.querySelector('.prev').addEventListener('click', () => {
 
 document.querySelector('.next').addEventListener('click', () => {
     const imageSrc = disImg.getAttribute('src')
-    const i = (images.indexOf(imageSrc.split('/')[1]) + 1) % 5;
+    const i = (images.indexOf(imageSrc.split('/')[1]) + 1) % 6;
     const image = `img/${images[i]}`;
 
     disImg.setAttribute('src', image);
