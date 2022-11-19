d4f17920624e5ab60dfad025a9a2ca5e08a983c0
Osi, Keme <108202263+Kemelite1@users.noreply.github.com>
2022-11-19 12:39:00 +0100

    6 images

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const heading = document.querySelector('h1');
 const disImg = document.querySelector('.displayed-img');
 
-const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg' 'img6.jpg'];
-const altText = ['img1', 'img2', 'img3', 'img4', 'img5' 'img6'];
+const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg'];
+const altText = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6'];
 
 document.querySelector('.prev').addEventListener('click', () => {
     const imageSrc = disImg.getAttribute('src')
