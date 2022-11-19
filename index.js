e933a7c9e48c84a9c7721a017e3bfc944eb13e2c
Osi, Keme <108202263+Kemelite1@users.noreply.github.com>
2022-11-19 01:21:05 +0100

    second corrections

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const disImg = document.querySelector('.displayed-img');
 const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'];
 const altText = ['img1', 'img2', 'img3', 'img4', 'img5'];
 
-// img/img1.jpg
-// img/img2.jpg
-// img/img3.jpg
-// img/img4.JPG
-// mg/img5.jpg
-// img/img6.jpg
 
 document.querySelector('.next').addEventListener('click', () => {
     const imageSrc = disImg.getAttribute('src')
